c12aed8721dc8858dfcb40cb160e816d7e7e31dc
Kelp <kelp@phate.org>
2020-05-04 06:57:18 +0800

    test: add tests for job handler

--- __tests__/handlers/job-handler.js
+++ __tests__/handlers/job-handler.js
@@ -5,6 +5,7 @@ const redis = require('redis');
 const errors = require('../../lib/models/errors');
 const Request = require('../../lib/models/request');
 const Response = require('../../lib/models/response');
+const JobState = require('../../lib/models/constants/job-state');
 const jobHandler = require('../../lib/handlers/job-handler');
 let queues;
 
@@ -36,9 +37,102 @@ const generateRequest = ({method, url}) => {
 const generateResponse = requestId => {
   return new Response({
     requestId,
-    ws: {}
+    ws: {
+      send: () => {}
+    }
   });
 };
+
+describe('clean jobs', () => {
+  test('can not clean waiting jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.WAITING}`
+    });
+    const response = generateResponse(request.id);
+    const fn = () => jobHandler.cleanJobs(request, response, () => {}, 'test');
+
+    expect(fn).toThrowErrorMatchingSnapshot();
+  });
+
+  test('can not clean active jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.ACTIVE}`
+    });
+    const response = generateResponse(request.id);
+    const fn = () => jobHandler.cleanJobs(request, response, () => {}, 'test');
+
+    expect(fn).toThrowErrorMatchingSnapshot();
+  });
+
+  test('can not clean delayed jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.DELAYED}`
+    });
+    const response = generateResponse(request.id);
+    const fn = () => jobHandler.cleanJobs(request, response, () => {}, 'test');
+
+    expect(fn).toThrowErrorMatchingSnapshot();
+  });
+
+  test('can not clean paused jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.PAUSED}`
+    });
+    const response = generateResponse(request.id);
+    const fn = () => jobHandler.cleanJobs(request, response, () => {}, 'test');
+
+    expect(fn).toThrowErrorMatchingSnapshot();
+  });
+
+  test('clean jobs with failed queue name', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.COMPLETED}`
+    });
+    const response = generateResponse(request.id);
+    const fn = () => jobHandler.cleanJobs(request, response, () => {}, 'not-found');
+
+    expect(fn).toThrowError(errors.Http404);
+  });
+
+  test('clean completed jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.COMPLETED}`
+    });
+    const response = generateResponse(request.id);
+
+    jest.spyOn(queues[0], 'clean');
+    jest.spyOn(response, 'json');
+
+    return jobHandler.cleanJobs(request, response, () => {}, 'test')
+      .then(() => {
+        expect(queues[0].clean).toBeCalledWith(0, JobState.COMPLETED);
+        expect(response.json).toBeCalledWith({}, 204);
+      });
+  });
+
+  test('clean failed jobs', () => {
+    const request = generateRequest({
+      method: 'POST',
+      url: `/queues/test/jobs/_clean?state=${JobState.FAILED}`
+    });
+    const response = generateResponse(request.id);
+
+    jest.spyOn(queues[0], 'clean');
+    jest.spyOn(response, 'json');
+
+    return jobHandler.cleanJobs(request, response, () => {}, 'test')
+      .then(() => {
+        expect(queues[0].clean).toBeCalledWith(0, JobState.FAILED);
+        expect(response.json).toBeCalledWith({}, 204);
+      });
+  });
+});
 
 describe('get job', () => {
   test('get a job with failed queue name', () => {
@@ -76,7 +170,7 @@ describe('get job', () => {
     return queues[0].add({data: 1}) // Add test job.
       .then(() => jobHandler.getJob(request, response, () => {}, 'test', '1'))
       .then(() => {
-        expect(response.json).toBeCalledWith(expect.any(Object));
+        expect(response.json).toBeCalled();
       });
   });
 
